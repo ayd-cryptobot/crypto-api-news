f1637589c9dfbaafd950e06cbfa0ba1f675c0ce2
sagarus2020 <juanpablocq@ufps.edu.co>
2022-10-30 14:43:36 -0500

    added event news

--- src/servicios/news.js
+++ src/servicios/news.js
@@ -12,70 +12,61 @@ var con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "root",
-  database: "sys"
+  database: "news"
 });
 
 //account variables
 var crypto_name;
     var user_id;
 
-
-endpoints.post('/news/follow',async(req,res)=> {
-    console.log(req.body,"este es el body")
-
-
-    //const buff = Buffer.from(req.body.message.data, 'base64');
-    //const buff = Buffer.from(req.body.message.data, 'base64');
-    const buff = req.body;
-    //const id=buff.toString('utf-8')
- const id=buff;
-//console.log(JSON.parse(id));
-    //  first_name=JSON.parse(id).first_name;
-    //  last_name=JSON.parse(id).last_name;
-    //  email=JSON.parse(id).email;
-    //  username=JSON.parse(id).username;
-    //  telegram_id=JSON.parse(id).chat_id;
-    //  password=JSON.parse(id).password;
-    //  rol=JSON.parse(id).rol;
-    crypto_name=id.crypto_name;
-    user_id=id.user_id;
-
-
-
-     con.connect(function(err) {
-      if (err) throw err;
-      console.log("Connected!");
-
-      var sql = "SELECT id_string FROM follow WHERE (crypto_name='"+crypto_name+"') AND (user_id='"+user_id+"')";
-      con.query(sql, function (err, result) {
-        if (err) {
-        console.log("err check");
-        //var sql = "INSERT INTO follow ('user_id','crypto_name') VALUES ('user_id',(SELECT user_id FROM user WHERE user_id='"+user_id+"')), ('crypto_name',(SELECT crypto_name FROM crypto WHERE crypto_name='"+crypto_name+"'));";
-        var sql = "INSERT INTO follow (user_id,crypto_name) VALUES ('"+user_id+"','"+crypto_name+"');";
-      
-      
-        con.query(sql, function (err, result) {
-          if (err) throw err;
-          console.log("1 follow inserted");
-          res.json({"message":"1 follow inserted"});
-        });
-      }
-      else{
-        console.log("del check");
-      var sql = "DELETE FROM follow WHERE (crypto_name='"+crypto_name+"') AND (user_id='"+user_id+"')";
+    function createFollow(user_id ,crypto_name){
+      let i=0;
+      while (i<crypto_name.length) {
+        var sql = "INSERT INTO follow (user_id,crypto_name) VALUES ('"+user_id+"','"+crypto_name[i]+"');";
       con.query(sql, function (err, result) {
         if (err) throw err;
-        console.log("1 follow deleted");
-        res.json({"message":"1 follow deleted"});
+        console.log("1 follow inserted");
       });
-    }
-
-      });
-
+        i++;
+      }
+  
       
-    });
-    res.end;
+    }
+    function resetFollow(user_id){
+     
+      var sql = "SELECT id_string FROM follow WHERE  (user_id='"+user_id+"')";
+        con.query(sql, function (err, result) {
+          if(err)return;
+        var sql = "DELETE FROM follow WHERE(user_id='"+user_id+"');";
+      con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 follow inserted");
+      });
+  })
+  
+      }
 
+endpoints.post('/news/follow',async(req,res)=> {
+  console.log(req.body,"este es el body")
+
+
+  //const buff = Buffer.from(req.body.message.data, 'base64');
+  //const buff = Buffer.from(req.body.message.data, 'base64');
+  const buff = req.body;
+  //const id=buff.toString('utf-8')
+const id=buff;
+
+  crypto_name=id.following_cryptos;
+  user_id=id.telegram_id;
+  currency_pair=id.currency_pair;
+   con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  res.json({"message":"follows inserted"});
+  resetFollow(user_id);
+  createFollow(user_id,crypto_name,currency_pair);
+
+})
 
 })
 
@@ -142,5 +133,81 @@ res.json({"mensaje":"no hay datos"})
 
 endpoints.get('/news/consult',async(req,res)=> {
   res.json({"mensaje":"criptobot.com/news/consult"})
+
+})
+
+endpoints.post('/news/accounts/event',async(req,res)=> {
+  console.log(req.body,"este es el body")
+
+
+  //const buff = Buffer.from(req.body.message.data, 'base64');
+  //const buff = Buffer.from(req.body.message.data, 'base64');
+  const buff = req.body.message.data;
+  //const id=buff.toString('utf-8')
+const id=buff;
+try{
+var operation_type=id.operation_type
+console.log(operation_type);
+}
+catch(err){
+}
+
+   con.connect(async function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+
+     switch(operation_type){
+
+  case("create"):
+ var telegram_user_id=id.telegram_user_id
+// var first_name=id.first_name
+// var last_name=id.last_name
+// var email=id.email
+// var username=id.username
+
+//     var sql = "INSERT INTO user (telegram_id,first_name, last_name,email  username,  rol) VALUES ('"+telegram_user_id+"','"+first_name+"','"+ last_name+"','"+ email+"','"+ username+"','cliente');";
+var sql = "INSERT INTO user (telegram_id) VALUES ('"+telegram_user_id+"');";
+   
+con.query(sql,async function (err, result) {
+    if (err) throw err;
+
+   await res.json({"message":"account created"});
+  });
+  break;
+  case("edit"):
+  var telegram_user_id=id.telegram_user_id
+// var first_name=id.first_name
+// var last_name=id.last_name
+// var email=id.email
+// var username=id.username
+//     var sql = "UPDATE user SET first_name='"+first_name+"',last_name='"+ last_name+"',email='"+ email+"',username='"+ username+"' WHERE (telegram_id='"+telegram__user_id+"');";
+var query_schedule=id.query_schedule;
+
+var sql = "UPDATE user SET query_schedule='"+ query_schedule+"' WHERE (telegram_id='"+telegram__user_id+"');";
+   
+con.query(sql,async  function (err, result) {
+
+    await res.json({"message":"account edited"});
+  });
+  break;
+  case("delete"):
+  var telegram_user_id=id.telegram_user_id
+    var sql = "DELETE FROM user WHERE (telegram_id='"+telegram_user_id+"');";
+  con.query(sql, async function (err, result) {
+    if (err) throw err;
+
+    await res.json({"message":"account deleted"});
+  });
+  break;  
+  default:
+
+   await res.json({"error":"event not found"});
+    break;
+   }
+
+res.end
+
+
+})
 
 })
